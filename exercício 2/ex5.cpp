cd1184614066dfea32f93e5e6f8c8ebc632a42c7
muriloferreiraamorim <164514812+muriloferreiraamorim@users.noreply.github.com>
2024-04-01 15:51:46 -0300

    Add files via upload

---
+++ exercício 2/ex5.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	double larg,comp,area;
+ 
+    cout <<"Insira a largura da sala (m):"<< endl;
+    cin >> larg;
+ 
+    cout <<"Insira o comprimento da sala (m):"<< endl;
+    cin >> comp;
+ 
+ 
+    area = larg*comp;
+    cout <<"Area da sala: "<< area << " metros quadrados" << endl;
+
+return 0;
+}
